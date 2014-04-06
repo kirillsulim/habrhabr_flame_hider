b2753dc7a162550aba175b1b50d4dd91bac0917c
kirillsulim <kirillsulim@gmail.com>
2014-04-06 13:02:58 +0600

    Padding save on collapse Button change on click

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 arr = $('.comment_item');
 for(i=0; i < arr.length; i++) 
 	$(arr[i]).children('.comment_body').children('.info').children('.clear ').before(
-		'<div>'+
-			'<a href=\"javascript:$(\'#'+arr[i].id+'\').children(\'.reply_comments\').hide();\"'+
-			'alt=\"Свернуть комментарии\">[-]</a>' +
-			'<a href=\"javascript:$(\'#'+arr[i].id+'\').children(\'.reply_comments\').show();\"'+
-			'alt=\"Развернуть комментарии\">[+]</a>'+
-			'</div>'
+		'<div style="padding-top: 3px;text-decoration: none;">'+
+			'<a href=\"javascript:$(\'#'+arr[i].id+'\').children(\'.reply_comments\').hide();' + 
+			'$(\'#'+arr[i].id+'\').css(\'margin-bottom\',\'20px\');' +
+			'$(\'#'+arr[i].id+'_hide\').hide();' +
+			'$(\'#'+arr[i].id+'_show\').show();\" ' +
+			'id="'+ arr[i].id +'_hide" ' +
+			'style="text-decoration: none;margin-left: 5px;">[-]</a>' +
+			'<a href=\"javascript:$(\'#'+arr[i].id+'\').children(\'.reply_comments\').show();' +
+			'$(\'#'+arr[i].id+'\').css(\'margin-bottom\',\'0px\');' +
+			'$(\'#'+arr[i].id+'_hide\').show();' +
+			'$(\'#'+arr[i].id+'_show\').hide();\" ' +
+			'id="'+ arr[i].id +'_show" ' +
+			'style="text-decoration: none;margin-left: 5px; display: none;">[+]</a>' +
+		'</div>'
 	);
